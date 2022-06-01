03b92d179f1d42bd398bf8df3454e2f8f8f5719e
daniyal <daniyalakbar1217@gmail.com>
2022-06-01 06:33:31 +0500

    First Commit

---
+++ Flask_Hbase_Docker_Swarm/flask_webapp/Hbase_API.py
@@ -0,0 +1,119 @@
+from operator import contains
+import happybase
+import csv
+from numpy import product
+
+from sympy import Product
+
+connection = happybase.Connection('hbase-docker', port=9090, autoconnect=True)
+
+# FETCHING LIST OF ALL TABLES IN DB
+
+
+def fetch_table():
+    return connection.tables()
+
+
+# SEACHING FOR A PRODUCT
+def search_products(search_text="SHIRT"):
+    table = connection.table('productss')
+    products = []
+
+    for key, data in table.scan():
+        if len(products) == 6: break
+        if search_text in str(data):
+            id = key.decode('utf-8')
+            _product = [id]
+            for val1, val2 in data.items():
+                val1 = val2.decode('utf-8')
+                _product.append(val1)
+                # print(f"{id} ===>>> {val1}")
+            products.append(_product)
+    # print(F"===================================>>>>>>>>>>>>>>>> search_text: {search_text}, output: {products}")
+    return products
+
+
+
+
+# SORTING PRODUCTS BY LOW PRICE
+def filter_low_price(search_text="CLOCK"):
+    products = search_products(search_text)
+    # for i in products: print(i[-2])
+    elm = 4
+    products.sort(key=lambda x: x[elm])
+    # print('=============>>>>> ', products)
+    # for i in products: print(i)
+    return products
+filter_low_price()
+
+
+
+# SORTING PRODUCTS BY LATEST
+def filter_latest(search_text="METAL"):
+    products = search_products(search_text)
+    # for i in products: print(i[2])
+    elm = 2
+    products.sort(key=lambda x: x[elm])
+    # print('=============')
+    # for i in products: print(i)
+    return products
+
+
+
+
+# FETCHING RELATED PRODUCTS
+def related_products(CustomerID='16425'):
+    table = connection.table('productss')
+    products = []
+
+    for key, data in table.scan():
+        if len(products) == 5: break
+        if CustomerID in str(data):
+            id = key.decode('utf-8')
+            _product = [id]
+            for val1, val2 in data.items():
+                val1 = val2.decode('utf-8')
+                _product.append(val1)
+            products.append(_product)
+    return products
+
+
+# FETCHING RECORDS ACCORDING TO SEASON
+def seasonal_products(season="Summer"):
+    global months
+    if season == "Summer":
+        months = [5, 6, 7]
+    elif season == "Winter":
+        months = [12, 1, 2]
+    elif season == "Autumn":
+        months = [9, 10, 11]
+    elif season == "Spring":
+        months = [3, 4, 5]
+
+    table = connection.table('products')
+    products = []
+
+    for key, data in table.scan():
+        id = key.decode('utf-8')
+        _product = [id]
+        for val1, val2 in data.items():
+            val1 = val2.decode('utf-8')
+            _product.append(val1)
+        products.append(_product)
+    products.pop()
+
+    for _record in products:
+        _month = str(_record[1]).split('/')
+        if int(_month[1]) not in months:
+            products.remove(_record)
+    return products[0:6]
+
+
+# DETECTION SPAM MESSAGES
+def detect_spam(message="WINNER"):
+    table = connection.table('spam')
+
+    for key, data in table.scan():
+        if message in str(data):
+            return "SPAM"
+    return "NOT SPAM"
---
+++ Flask_Hbase_Docker_Swarm/flask_webapp/view.py
@@ -0,0 +1,90 @@
+# from urllib import response
+from flask import Flask, url_for, redirect, render_template, request, session
+import os
+import Hbase_API as hbase
+
+app = Flask(__name__)
+app.secret_key = 'Programatically'
+
+
+@app.route('/', methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        pass
+    else:
+        return render_template('index.html')
+
+
+
+@app.route('/service-page', methods=["POST", "GET"])
+def service_page():
+    if request.method == "POST":
+        search_text = request.form["search"]
+        session['search_text'] = search_text
+        radio_filter1 = str(request.form.get("radio_filter1"))
+        radio_filter2 = str(request.form.get("radio_filter2"))
+        
+        if radio_filter1 == 'None' and radio_filter2 == 'None':
+            records = hbase.search_products(search_text)
+            return render_template('service-page.html', response = records)
+        elif radio_filter1 == 'on':
+            records = hbase.filter_low_price(search_text)
+            return render_template('service-page.html', response = records)
+        elif radio_filter2 == 'on':
+            records = hbase.filter_latest(search_text)
+            return render_template('service-page.html', response = records)
+        else:
+            records = hbase.search_products(search_text)
+            return render_template('service-page.html', response = records)
+    else:
+        return render_template('service-page.html')
+
+
+
+
+
+@app.route('/seasonal_products', methods=["POST", "GET"])
+def seasonal_products():
+    if request.method == "POST":
+        # print("=============>>>>>>>>>", request.form.get(list(request.form.keys())[0]))
+        # print("=============>>>>>>>>>", list(request.form.keys())[0])
+        products = hbase.seasonal_products(str(list(request.form.keys())[0]))
+        # records = hbase.search_products(search_text)
+        return render_template('seasonal_products.html', response = products)
+    else:
+        return render_template('seasonal_products.html')
+
+
+
+
+@app.route('/product_page', methods=["POST", "GET"])
+def product_page(param = "Hello World!"):
+    if request.method == "GET":
+        print('==================>>>>>>>>>>>>> YOU ENTERED GET METHOD ')
+        name = request.args.get('name')
+        price = F"Price: ${request.args.get('price')}"
+        custID = request.args.get('custID')
+
+        records = hbase.search_products(custID)
+        return render_template('product-page.html', response = records, _record=[price, name])
+    else:
+        return render_template('product-page.html')
+
+
+
+
+@app.route('/contact', methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        message = request.form["message"]
+        is_spam = hbase.detect_spam(message)
+        return render_template('contact-us.html', response = is_spam)
+    else:
+        return render_template('contact-us.html')
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
